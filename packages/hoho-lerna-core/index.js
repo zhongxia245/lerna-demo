4d50b57707a745626df4d29e98a1e9570e8bd22a
zhongxia245 <zhongxia245@sina.com>
2018-11-06 22:32:18 +0800

    增加单元测试 hoho-lerna-core

--- packages/hoho-lerna-core/index.js
+++ packages/hoho-lerna-core/index.js
@@ -1,1 +1,8 @@
+var add = require('./lib/add')
+var sub = require('./lib/sub')
 console.log('hoho-lerna-core')
+
+module.exports = {
+  add: add,
+  sub: sub
+}
---
+++ packages/hoho-lerna-core/test/index.test.js
@@ -0,0 +1,32 @@
+var core = require('../index')
+var expect = require('chai').expect
+
+console.log(core)
+
+describe('hoho-lerna-core test', function() {
+  //defer对象测试
+  describe('add.js test', function() {
+    // success
+    it('3 + 4 = 7', function() {
+      expect(core.add(3, 4)).to.be.equal(7)
+    })
+
+    // error
+    it('3 + 3 != 7', function() {
+      expect(core.add(3, 3)).not.to.equal(7)
+    })
+  })
+
+  //defer对象测试
+  describe('sub.js test', function() {
+    // success
+    it('4 - 3 = 1', function() {
+      expect(core.sub(4, 3)).to.be.equal(1)
+    })
+
+    // error
+    it('4 - 2 != 1', function() {
+      expect(core.sub(4, 2)).not.be.equal(1)
+    })
+  })
+})
